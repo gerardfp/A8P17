db642adf784ccf5d584e3ad22fb54f7dd487b10b
gerard <gerard@gerard>
2018-05-18 20:12:19 +0200

    filechannel.read

---
+++ src/com/company/Main22.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import com.company.manager.ManagerEquips2;
+import com.company.model.Equip;
+
+public class Main22 {
+
+    public static void main(String[] args) {
+
+        ManagerEquips2.inscriureEquip("abcd");
+        ManagerEquips2.obtenirEquip(1);
+//        ManagerEquips2.inscriureEquip("BBB");
+//        ManagerEquips2.inscriureEquip("CCCCCCC");
+//        // borrar 2
+//        ManagerEquips2.inscriureEquip("DDDDDDD");
+
+    }
+}
